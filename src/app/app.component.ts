8cf083654138f6dd58704538ef0d498679d1c47c
Chandar <Phani.Chandar@dcs.IN.gov>
2018-02-20 15:21:51 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import { ProductService } from './Products/product.service';
+
+@Component({
+selector:'pm-root',
+template:`
+<div><h1>{{pageTitle}}</h1>
+<pm-products></pm-products>
+</div>`,
+providers : [ProductService]
+})
+
+export class AppComponent{
+
+pageTitle :string = 'Acme Product Management2';
+
+}
